cabbd1bbb5016e08d5ae572d77b16f842bd4eb0d
Marcus Riemer <Marcus@GurXite.de>
2015-02-19 07:53:49 +0100

    Now using okular for TeX-view

--- init.el
+++ init.el
@@ -75,6 +75,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Okular")
+     (output-html "xdg-open"))))
  '(adaptive-wrap-extra-indent 4)
  '(ecb-layout-window-sizes
    (quote
