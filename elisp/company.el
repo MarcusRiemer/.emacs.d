2ad0367ee833a776a391074409e0f031889db2fc
Marcus Riemer <Marcus@GurXite.de>
2016-02-17 12:55:51 +0100

    Additonal key bindings
    * C-SPC to trigger autocomplete
    * M-y to review the kill ring
    * Remapped C-x C-f to use helm

--- elisp/company.el
+++ elisp/company.el
@@ -1,2 +1,5 @@
 ;; Activate company mode globally
 (add-hook 'after-init-hook 'global-company-mode)
+
+;; Complete on C-SPC
+(global-set-key (kbd "C-SPC") 'company-complete)
--- elisp/helm-projectile.el
+++ elisp/helm-projectile.el
@@ -6,3 +6,5 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-r") 'helm-projectile)
 (global-set-key (kbd "C-t") 'helm-semantic-or-imenu)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
